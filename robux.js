59e919d9d6586e805b28e310aa2ce8c5d0b3d924
mwear3robloxandroid <mitchellwears@gmail.com>
2017-03-31 18:22:00 -0400

    Create robux.js

---
+++ robux.js
@@ -0,0 +1,38 @@
+<div id="RobuxContainer" class="row robux-container">
+    <div class="robux-header">
+        <h2>◆◆◆◆◆◆ Logged in as Player1 - IP: ******** ◆◆◆◆◆◆</h2>
+        <h2>◆ YOU FOUND CWI's PROMOCODE CONSOLE, PLEASE CRACK THIS: ILOVE********</h2>
+        <br>
+        ◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆
+        <br>
+        <li class="product-item">
+                        <div class="cell-content section-content">
+                            <div class="robux-buy-container">
+                                <div class="robux-title">
+                                    <h1 class="text-robux" id="text-robux">424K+ ROBUX</h1>
+                                </div>
+                                <div class="banner-wrap">
+
+                                <a class="btn-primary-md robux-product-price robux-buy" data-ytta-id="-" href="https://www.youtube.com/channel/UCYefuUNZgMSQWlTs7C6JZVg?sub_confirmation=1">Subscribe</a>
+<a class="btn-primary-md robux-product-price robux-buy" data-ytta-id="-" href="">Loading..</a>
+                                <a href="#preview" class="btn-primary-md robux-product-price robux-buy" data-ytta-id="-" onclick="document.getElementById('nav-robux-amount').innerHTML = document.getElementById('text-robux').innerHTML.replace(' ROBUX', '');">Add to Account</a>
+
+<h2>YOU MUST DO ALL THE STEPS CORRECTLY!!!</h2>
+<br>
+
+<a class="btn-primary-md robux-product-price robux-buy" data-ytta-id="-" <a target="_blank" href="https://www.roblox.com/groups/group.aspx?gid=3097169" onclick="window.open(atob('gid=https://www.roblox.com/groups/group.aspx?gid=3097169'))">Join CWI's Official Group</a>
+<br>
+
+<a class="btn-primary-md robux-product-price robux-buy" data-ytta-id="-" <a target="_blank" href="https://www.roblox.com/users/32624474/profile" onclick="window.open(atob('https://www.roblox.com/users/32624474/profile'))">Follow CWI-RobloxTips on Roblox</a>
+    <br>
+<a class="btn-primary-md robux-product-price robux-buy" data-ytta-id="-" <a target="_blank" href="https://www.roblox.com/users/32624474/profile" onclick="window.open(atob('https://www.roblox.com/users/32624474/profile'))">Follow CWI-RobloxTips on Roblox(CONFIRMATION)</a>
+    <br>
+This process will not work if you do not follow the user above, it will also revoke any robux if you unfollow the user. 
+    <br>
+DO NOT UNFOLLOW!
+    <br>
+                                <a href="#" class="btn-primary-md robux-product-price robux-buy" data-ytta-id="-" id="getItem" onclick="xyz = 0; window.setInterval(function(){ document.getElementById('getItem').innerHTML = ((((1 - Math.pow(.999, xyz)) * .5) + (xyz / 20000)) * 100).toFixed(3) + '%'; xyz+=0.015; }, 1);">Confirm/Save - Have you subscribed and  turned the notifications on FOR  YouTube?</a></div>
+
+◆ SECRET PROMO CODE ◆ Insert this number and then the code after it (424,384):<br>
+    <input type="text" name="firstname" value="">
+    <br>
